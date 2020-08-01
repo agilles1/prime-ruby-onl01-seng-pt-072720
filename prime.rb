2f2cc980f3a7191e89bc220b568b6af6b2d60052
agilles1 <adam.g.tpt@gmail.com>
2020-07-31 20:41:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+ 
+def prime?(int)
+   range = Range.new(2, int-1)
+   range_array = range.to_a
+   if int <= 1 
+    false
+   else
+    range_array.none? {|num| int % num == 0}
+   end
+end 
